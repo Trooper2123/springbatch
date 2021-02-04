12a3bf0c2f67d8a527769119366b20329a994c52
Trooper2123 <tete.leticia@gmail.com>
2021-02-04 12:48:45 -0300

    initial

---
+++ src/main/java/com/trooper2123/springbatchstudy/SpringbatchStudyApplication.java
@@ -0,0 +1,13 @@
+package com.trooper2123.springbatchstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbatchStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trooper2123/springbatchstudy/SpringbatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trooper2123.springbatchstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
