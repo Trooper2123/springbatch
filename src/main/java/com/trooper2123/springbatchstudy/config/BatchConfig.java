b21177aa9feee3f96c91918936e4d13a2cf4841b
Trooper2123 <tete.leticia@gmail.com>
2021-02-05 19:40:56 -0300

    Even or Odd with chunk process.

--- src/main/java/com/trooper2123/springbatchstudy/config/BatchConfig.java
+++ src/main/java/com/trooper2123/springbatchstudy/config/BatchConfig.java
@@ -11,11 +11,19 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.batch.api.chunk.ItemReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableBatchProcessing
@@ -29,30 +37,36 @@ public class BatchConfig {
 
 
     @Bean
-    public Job printHelloJob() {
+    public Job printOddOrEvenJob() {
         return jobBuilderFactory
-                .get("printHelloJob")
-                .start(printHelloStep())
+                .get("printOddOrEvenJob")
+                .start(printOddOrEvenStep())
                 .incrementer(new RunIdIncrementer())
                 .build();
     }
 
-    public Step printHelloStep() {
+    public Step printOddOrEvenStep() {
         return stepBuilderFactory
-                .get("printHelloAStep")
-                .tasklet(helloTasklet(null))
+                .get("printOddOrEvenStep")
+                .<Integer, String>chunk(1)
+                .reader(countUntillTenReader())
+                .processor(oddOrEvenProcessor())
+                .writer(printWriter())
                 .build();
     }
 
-    @StepScope
-    @Bean
-    public Tasklet helloTasklet(@Value("${job_parameter_name}") String name) {
-        return new Tasklet() {
-            @Override
-            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                System.out.println(String.format("Hello,%S",name));
-                return RepeatStatus.FINISHED;
-            }
-        };
+    public IteratorItemReader<Integer> countUntillTenReader(){
+        List<Integer> numberUntilTen = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        return new IteratorItemReader<Integer>(numberUntilTen.iterator());
+    }
+
+    public FunctionItemProcessor<Integer,String> oddOrEvenProcessor(){
+        return new FunctionItemProcessor<Integer,String>(
+                item -> item % 2 ==0? String.format("Item %s is Even",item): String.format("Item %s is Odd",item)
+        );
+    }
+
+    public ItemWriter<String> printWriter(){
+        return itens -> itens.forEach(System.out::println);
     }
 }
