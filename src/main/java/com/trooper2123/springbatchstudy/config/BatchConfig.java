0e827cebcb9670f2dfc7f1d258ae9a4d6dc7368d
Trooper2123 <tete.leticia@gmail.com>
2021-02-05 18:49:34 -0300

    Execute same tasklet multiple times (incrementer).
    Use env var inside tasklet.

--- src/main/java/com/trooper2123/springbatchstudy/config/BatchConfig.java
+++ src/main/java/com/trooper2123/springbatchstudy/config/BatchConfig.java
@@ -7,10 +7,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,19 +33,26 @@ public class BatchConfig {
         return jobBuilderFactory
                 .get("printHelloJob")
                 .start(printHelloStep())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
     public Step printHelloStep() {
         return stepBuilderFactory
                 .get("printHelloAStep")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("Hello!");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
+                .tasklet(helloTasklet(null))
                 .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet helloTasklet(@Value("${job_parameter_name}") String name) {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println(String.format("Hello,%S",name));
+                return RepeatStatus.FINISHED;
+            }
+        };
     }
 }
