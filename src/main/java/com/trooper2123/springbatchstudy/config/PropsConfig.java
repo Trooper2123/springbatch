bbea3751db126e43beb22132bf97fe292541c642
Trooper2123 <tete.leticia@gmail.com>
2021-02-10 12:31:14 -0300

    enable 2 data sources

--- src/main/java/com/trooper2123/springbatchstudy/config/PropsConfig.java
+++ src/main/java/com/trooper2123/springbatchstudy/config/PropsConfig.java
@@ -1,10 +1,13 @@
 package com.trooper2123.springbatchstudy.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.FileSystemResource;
 
-public class PropsCanfig {
-
+@Configuration
+public class PropsConfig {
+    @Bean
     public PropertySourcesPlaceholderConfigurer config() {
         PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
         configurer.setLocation(new FileSystemResource("src/main/resources/application.properties"));
